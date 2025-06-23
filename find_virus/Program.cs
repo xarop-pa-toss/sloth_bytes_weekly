de5296ccd75500046146c0598c91c52ca5552815
xarop-pa-toss <rpgdsantos@gmail.com>
2025-06-23 03:06:03 +0100

    Create Find_Virus

---
+++ find_virus/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+string RemoveVirus(string filesIn)
+{
+    string files = filesIn.Replace("PC Files: ", "");
+    if (files == null || files.Equals(filesIn))
+    {
+        throw new ArgumentException("File list does not start with 'PC Files: '");
+    }
+
+    List<string> wordsToDelete = ["virus", "malware"];
+    List<string> excludedWords = ["anti", "not"];
+
+    string newFiles = string.Join(", ",
+        files
+        .Split(", ")
+        .Where(filename =>
+            !(
+                wordsToDelete.Any(w => filename.Contains(w, StringComparison.OrdinalIgnoreCase)) &&
+                !excludedWords.Any(w => filename.Contains(w, StringComparison.OrdinalIgnoreCase))
+            )
+        )
+    );
+
+    return "PC Files: " +  newFiles;
+}
+
+
+Console.WriteLine(RemoveVirus("PC Files: spotifysetup.exe, virus.exe, dog.jpg"));
+Console.WriteLine(RemoveVirus("PC Files: antivirus.exe, cat.pdf, lethalmalware.exe, dangerousvirus.exe "));
+Console.WriteLine(RemoveVirus("PC Files: notvirus.exe, funnycat.gif"));
